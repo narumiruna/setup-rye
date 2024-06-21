247f61f717524a139fb7b7a7c9f7505e1c151b91
なるみ <4680567+narumiruna@users.noreply.github.com>
2024-06-21 17:48:38 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 curl -sSf https://rye.astral.sh/get | bash
 
-# echo "$HOME/.rye/shims" >> $GITHUB_PATH
-export PATH="$HOME/.rye/shims:$PATH"
-# source "$HOME/.rye/env"
+echo "$HOME/.rye/shims" >> $GITHUB_PATH
+# export PATH="$HOME/.rye/shims:$PATH"
